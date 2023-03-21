7f851f66823e8af6f9f9c5939b0dbc106ffd9991
Rajveer-2009 <84115638+Rajveer-2009@users.noreply.github.com>
2023-03-21 15:31:25 +0530

    Update main.js

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
+noseX = 0;
+noseY = 0;
+rightWristX = 0;
+leftWristX = 0;
+diffrence = 0;
+
 function setup() {
     video = createCapture(VIDEO);
-    video.size(550, 500);
-    canvas = createCanvas(550, 550);
-    canvas.position(560, 150);
+    video.size(500, 500);
+    video.position(20, 75);
+    canvas = createCanvas(500, 500);
+    canvas.position(560, 75);
     poseNet = ml5.poseNet(video, modleLoaded);
-    poseNet.on('pose', gotPoses)
+    poseNet.on('pose', gotPoses);
 }
 
 function modleLoaded() {
@@ -14,9 +21,19 @@ function modleLoaded() {
 function gotPoses(results) {
     if (results.length > 0) {
         console.log(results);
+        noseX = results[0].pose.nose.x;
+        noseY = results[0].pose.nose.y;
+        console.log("Nose X: " + noseX + "Nose Y:" + noseY);
+        leftWristX = results[0].pose.leftWrist.x;
+        rightWristX = results[0].pose.rightWrist.x;
+        console.log("left Wrist X: " + leftWristX + "right Wrist X:" + rightWristX);
+        diffrence = Math.floor(leftWristX - rightWristX);
     }
 }
 
 function draw() {
     background('#40C9FF');
+    fill("#ff0000");
+    stroke("black");
+    square(noseX, noseY, diffrence);
 }
